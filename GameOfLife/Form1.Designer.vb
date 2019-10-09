8eb33821487d85e6fee651494d27393f20db1933
Eric VONARX <eric.vonarx@free.fr>
2019-10-09 17:16:14 +0200

    Ajoutez des fichiers projet.

---
+++ GameOfLife/Form1.Designer.vb
@@ -0,0 +1,46 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class GameOfLife
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'GameOfLife
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.GameOfLife.My.Resources.Resources.univers3
+        Me.ClientSize = New System.Drawing.Size(1484, 861)
+        Me.Name = "GameOfLife"
+        Me.Text = "Game of life"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ GameOfLife/Form1.vb
@@ -0,0 +1,185 @@
+﻿Public Class GameOfLife
+
+    Dim iSize As Integer = 30
+    Dim aInitialPositions(iSize, iSize) As Integer
+    Dim aFinalPositions(iSize, iSize) As Integer
+
+    Private Sub GameOfLife_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        resetArrays()
+
+        'initial positions
+        Randomize()
+        For i = 0 To 200
+            aInitialPositions((Int(iSize - 1) * Rnd()), (Int(iSize - 1) * Rnd())) = 1
+        Next
+
+        'aInitialPositions(1, 2) = 1
+        'aInitialPositions(2, 1) = 1
+        'aInitialPositions(2, 2) = 1
+        'aInitialPositions(3, 2) = 1
+
+        'aInitialPositions(5, 2) = 1
+        'aInitialPositions(5, 3) = 1
+        'aInitialPositions(5, 4) = 1
+        'aInitialPositions(5, 5) = 1
+
+        'create PictureBoxes dynamically  
+        createPictureBoxes()
+
+        'Set and start a Timer
+        Timer1.Interval = 100
+        Timer1.Enabled = True
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As System.EventArgs) Handles Timer1.Tick
+
+        For i = 0 To iSize - 1
+            For j = 0 To iSize - 1
+                algoGameOfLife(i, j)
+            Next
+        Next
+
+        copyFinalArrayIntoInitialArray()
+        updateDisplay()
+    End Sub
+
+    Private Sub algoGameOfLife(ByVal line As Integer, ByVal column As Integer)
+
+        Dim nbAliveCells As Integer = 0
+
+        nbAliveCells = howManyAliveCellsAround(line, column)
+        ' A death cell that has at least 3 alive cells around it becomes alive
+        If (nbAliveCells = 3) And (Not isCellAlive(line, column)) Then
+            aFinalPositions(line, column) = 1
+        End If
+
+        'An alive cell that has 2 or 3 alive cells around it stays alive else it dies
+        If (isCellAlive(line, column)) Then
+            If nbAliveCells = 2 Or nbAliveCells = 3 Then
+                'the cell stays alive
+                aFinalPositions(line, column) = 1
+            Else
+                'the cell dies
+                aFinalPositions(line, column) = 0
+            End If
+        End If
+
+    End Sub
+
+
+    'Create dynamically as many pictureboxes as defined
+    'Each picturebox contains 1 pixel
+    Private Sub createPictureBoxes()
+
+        For i = 0 To iSize - 1
+            For j = 0 To iSize - 1
+                Dim obj As New System.Windows.Forms.PictureBox
+
+                With obj
+                    .Name = "PB" & i & j 'nom de ta picturebox (PB1, PB2, PB3, ...)
+                    .Left = (j - ((j \ iSize) * iSize)) * 15 + 10 ' 'position par rapport au rebord gauche de l'UserForm
+                    .Top = (i) * 15 + 10   'position par rapport au haut de l'UserForm
+                    .Width = 10 'largeur de la zone d'écriture
+                    .Height = 10 'hauteur de la zone d'écriture
+                    'tu peux rajouter ou enlever des propriétés de l'objet
+                    '.Text = "A"
+                    If aInitialPositions(i, j) <> 0 Then
+                        .BackColor = Color.Blue
+                    Else
+                        .BackColor = Color.Black
+                    End If
+                    .Visible = True
+
+                End With
+
+                'ajout du controle à la PictureBox1
+                Me.Controls.Add(obj)
+            Next
+        Next
+    End Sub
+
+    Private Sub updateDisplay()
+
+        Dim i As Integer = 0
+        Dim j As Integer = 0
+
+        For i = 0 To iSize - 1
+            For j = 0 To iSize - 1
+                If aInitialPositions(i, j) = 1 Then
+                    CType(Me.Controls("PB" & i & j), PictureBox).BackColor = Color.Blue
+                Else
+                    CType(Me.Controls("PB" & i & j), PictureBox).BackColor = Color.Black
+                End If
+            Next
+        Next
+
+    End Sub
+
+    Private Sub copyFinalArrayIntoInitialArray()
+        For k = 0 To iSize - 1
+            For l = 0 To iSize - 1
+                aInitialPositions(k, l) = aFinalPositions(k, l)
+            Next
+        Next
+    End Sub
+
+    Private Sub resetArrays()
+        For k = 0 To iSize - 1
+            For l = 0 To iSize - 1
+                aInitialPositions(k, l) = 0
+                aFinalPositions(k, l) = 0
+            Next
+        Next
+
+    End Sub
+    Private Function isCellAlive(ByVal param1 As Integer, ByVal param2 As Integer) As Boolean
+        Dim result As Boolean = False
+
+        If (aInitialPositions(param1, param2) <> 0) Then result = True
+
+        Return result
+
+    End Function
+
+
+    Private Function howManyAliveCellsAround(ByVal line As Integer, ByVal column As Integer) As Integer
+
+        Dim result As Integer = 0
+        Dim saveParam1 = line
+        Dim saveParam2 = column
+
+        For i = -1 To 1
+            For j = -1 To 1
+
+                line = saveParam1 + i
+                If (line = -1) Then
+                    line = iSize - 1
+                End If
+                If (line = iSize) Then
+                    line = 0
+                End If
+
+                column = saveParam2 + j
+                If (column = -1) Then
+                    column = iSize - 1
+                End If
+                If (column = iSize) Then
+                    column = 0
+                End If
+
+                If (aInitialPositions(line, column) <> 0) Then
+                    result = result + 1
+                End If
+            Next
+        Next
+        If (aInitialPositions(saveParam1, saveParam2) <> 0) Then result = result - 1
+
+        Return result
+
+    End Function
+
+
+End Class
